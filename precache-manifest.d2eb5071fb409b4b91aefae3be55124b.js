bb329f228b99ad062d60abd8f6c3c12d169c379b
vijay131094 <94071792+vijay131094@users.noreply.github.com>
2022-01-22 18:32:17 +0530

    Updates

---
+++ precache-manifest.d2eb5071fb409b4b91aefae3be55124b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4398d0c8a2bd26b190c33bd3605e7db",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "3f8d4af833165ca685bf",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "9a2c874b6a175a0122b7",
+    "url": "/box-office/static/js/2.e4522165.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.e4522165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f8d4af833165ca685bf",
+    "url": "/box-office/static/js/main.3f63f24c.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
